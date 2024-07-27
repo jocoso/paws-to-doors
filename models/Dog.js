5313ec884c76b90159d61efcdb1ccd5623a2c61c
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-07-26 23:31:12 -0400

    copied the same format for cat and added a dog model for dog.js

---
+++ models/Dog.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+    const Dog = sequelize.define('Dog', {
+      name: DataTypes.STRING,
+      imageUrl: DataTypes.STRING,
+      info: DataTypes.TEXT,
+      location: DataTypes.STRING,
+      breed: DataTypes.STRING 
+    });
+  
+    return Dog;
+  };
