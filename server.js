d739f7f31873334a3eeb681c3c6cfc4edf07d4e0
jocoso <joshua.collado022@gmail.com>
2024-07-29 19:59:09 -0400

    feat: Implement website display using Handlebars templates
    - Updated main layout and partials for consistent design.
    - Modified views to use Handlebars for dynamic content rendering.
    - Adjusted routes to pass necessary data to Handlebars templates.
    - Improved user experience by enhancing navigation and layout structure.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,23 +13,21 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 
 const sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {
-    maxAge: 24 * 60 * 60 * 1000, 
-    httpOnly: true, 
-    secure: process.env.NODE_ENV === 'production', 
-    sameSite: 'strict' 
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
   },
-  resave: false, 
-  saveUninitialized: false, 
+  resave: false,
+  saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
-    checkExpirationInterval: 15 * 60 * 1000, 
-    expiration: 24 * 60 * 60 * 1000  
+    db: sequelize
   })
 };
 
@@ -42,8 +41,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening to port ', PORT));
+  app.listen(PORT, () => console.log('Now listening to port: ', PORT));
 });
