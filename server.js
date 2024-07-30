a2b72fbe79bac7f0354d0ed7549ff20d22040e2b
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-07-30 14:26:52 -0400

    updated code so RescueGroupsAPI can function

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -48,3 +48,9 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
+
+
+
+
+
+
