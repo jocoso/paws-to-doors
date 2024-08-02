782e1c66b4b1dc307f751fda6c91d9037ad3594b
jocoso <joshua.collado022@gmail.com>
2024-08-01 22:27:24 -0400

    Removed several bugs resulted by inconsitent naming between the models and the schema as well of foreign keys.

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -1,34 +1,35 @@
-
-const User = require('../models/User');
+const User = require("../models/User");
 
 const userData = [
     {
-        username: 'john_doe',
-        email: 'john_doe@example.com',
-        password: 'password123',
+        username: "john_doe",
+        email: "john_doe@example.com",
+        password: "password123",
     },
     {
-        username: 'jane_smith',
-        email: 'jane_smith@example.com',
-        password: 'password456',
+        username: "jane_smith",
+        email: "jane_smith@example.com",
+        password: "password456",
     },
     {
-        username: 'mike_jones',
-        email: 'mike_jones@example.com',
-        password: 'password789',
+        username: "mike_jones",
+        email: "mike_jones@example.com",
+        password: "password789",
     },
     {
-        username: 'sara_brown',
-        email: 'sara_brown@example.com',
-        password: 'password101',
+        username: "sara_brown",
+        email: "sara_brown@example.com",
+        password: "password101",
     },
     {
-        username: 'alex_lee',
-        email: 'alex_lee@example.com',
-        password: 'password202',
+        username: "alex_lee",
+        email: "alex_lee@example.com",
+        password: "password202",
     },
 ];
 
-const seedUsers = () => User.bulkCreate(userData, { individualHooks: true });
+const seedUsers = async () => {
+    await User.bulkCreate(userData, { individualHooks: true });
+};
 
 module.exports = seedUsers;
