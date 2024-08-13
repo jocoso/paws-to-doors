24d7daf7dea251ecf2aab824ac1fe8fec3fee100
jocoso <joshua.collado022@gmail.com>
2024-08-12 20:49:27 -0400

    Fixed sign up/login error and cleaned up a little bit

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,37 +1,39 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
+  const name = document.querySelector('#username-signup').value.trim(); // Keeping the model and variable names consistent
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
-    const response = await fetch('/api/users/', {
+  if (name && email && password) {
+    const response = await fetch('/api/users/', { // Calling localhost:3001/api/users/...
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
+    
     if (response.ok) {
       await showAlert("Signup successful");
       document.location.replace('/');
     } else {
-      await showAlert("Signup failed");
+
+      await showAlert("Signup failed"); // Strange quiet error where the password will not be accepted sometimes for reasons...
     }
   }
 };
 
 function showAlert(message) {
-return new Promise((resolve) => {
-  const alertBox = document.getElementById('customAlert');
-  document.getElementById('alertMessage').textContent = message;
-  alertBox.style.display = 'block';
+  return new Promise((resolve) => {
+    const alertBox = document.getElementById('customAlert');
+    document.getElementById('alertMessage').textContent = message;
+    alertBox.style.display = 'block';
 
-  window.closeAlert = () => {
-    alertBox.style.display = 'none';
-    resolve();
-  };
-});
+    window.closeAlert = () => {
+      alertBox.style.display = 'none';
+      resolve();
+    };
+  });
 }
 
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
